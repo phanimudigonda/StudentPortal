51101bbe1d31baec69450e8b860bf97dbbfc1c01
phanimudigonda <81674506+phanimudigonda@users.noreply.github.com>
2022-03-20 15:40:05 +0530

    Update settings.py

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yykyr=5@*cdo3k_=vjc8r#!hfmy)v6tyk(()0q(svg%m6(j+87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',studentappportal.herokuapp.com]
 
 
 # Application definition
