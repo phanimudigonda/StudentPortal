cb5f3083bdeba934872e315d15def5e1becfc517
phanimudigonda <81674506+phanimudigonda@users.noreply.github.com>
2022-03-20 19:21:34 +0530

    Update urls.py

--- studentprofile/urls.py
+++ studentprofile/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
